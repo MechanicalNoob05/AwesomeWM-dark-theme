a3c5db011ad576b93c2b21a20822f8e2fe0587bb
Tejas Mayekar <tejasmayekar08@gmail.com>
2022-07-01 23:22:27 +0530

    install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Hello, hope you like this one"
+echo "Creating backup of existing files"
+zip Backup_config.zip ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/awesome
+
+cp -r picom/ ~/.config 
+cp -r polybar/ ~/.config 
+cp -r rofi/ ~/.config 
+cp -r awesome/ ~/.config
+
+echo "Do you want to remove the cloned directory"
+read option
+
+if [[ option = 'y' ]]; then
+  echo "Okay"
+  rm -r -v ~/AwesomeWM-dark-theme
+else 
+  echo "Okay"
+fi
